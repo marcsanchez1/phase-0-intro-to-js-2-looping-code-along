61e39c90247f0ad6654806a13293f60230b01e86
Marc <marcsanchez@me.com>
2022-03-12 23:33:53 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr, e) {
+  let names = [];
+  for (let i = 0; i < arr.length; i++) {
+    names.push(`Thank you, ${arr[i]} for the wonderful ${e} gift!`);
+  }
+  return names;
+}
+
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+let counter = 10;
+while (counter >= 0 ) {
+  console.log(counter);
+  counter--;
+}
+
